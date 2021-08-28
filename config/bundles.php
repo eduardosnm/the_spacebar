eb0cc989f9814efe5e07ecd7dd48f50a9644517c
Eduardo Bravo <eduardosnm@gmail.com>
2021-08-28 16:39:59 -0300

    instalacion de doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
