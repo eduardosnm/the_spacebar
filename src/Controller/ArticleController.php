1b284b9e18092c0f201a275bd4296c2a138adb34
Eduardo Bravo <eduardosnm@gmail.com>
2021-09-04 21:25:47 -0300

    recuperando datos de las relaciones

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
 use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
 use Doctrine\ORM\EntityManagerInterface;
@@ -49,17 +50,8 @@ class ArticleController extends AbstractController
             $slackClient->sendMessage("eduardo", "this is a test");
         }
 
-
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
-
-
         return $this->render('article/show.html.twig', [
             "article" => $article,
-            'comments' => $comments,
         ]);
     }
 
