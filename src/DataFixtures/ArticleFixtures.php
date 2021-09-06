02e1df82e1b7d82afe167f30e51b94bcc3661269
Eduardo Bravo <eduardosnm@gmail.com>
2021-09-05 21:39:50 -0300

    guardando relaciones many to many

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -4,9 +4,11 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Tag;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
+class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleImages = [
         'asteroid.jpeg',
@@ -31,10 +33,21 @@ EOF
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
 
+            /** @var Tag[] $tags */
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0,5));
+            foreach ($tags as $tag){
+                $article->addTag($tag);
+            }
 
         });
 
         $em->flush();
     }
 
+    public function getDependencies()
+    {
+        return [
+            TagFixture::class
+        ];
+    }
 }
